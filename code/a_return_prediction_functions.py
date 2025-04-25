0a0f2cfb68fa2cff349b16238405007a33b4f440
Theodorriese <theodor-riese@hotmail.com>
2025-04-25 08:42:11 +0200

    Push

--- code/a_return_prediction_functions.py
+++ code/a_return_prediction_functions.py
@@ -2,7 +2,6 @@ import numpy as np
 import pandas as pd
 from sklearn.linear_model import Ridge
 from sklearn.metrics import mean_squared_error
-import xgboost as xgb
 
 
 def data_split(data, val_end, val_years, train_start, train_lookback, retrain_lookback, test_inc, test_end):
