b35620af31438760e8818e9bea0654d6d296ef74
Theodorriese <theodor-riese@hotmail.com>
2025-05-02 09:02:49 +0200

    Push

--- code/i4_run_analysis.py
+++ code/i4_run_analysis.py
@@ -213,8 +213,8 @@ ar1_plot = compute_ar1_plot(
 ar1_plot.savefig(os.path.join(output_path, "ar1_plot.png"), bbox_inches="tight", dpi=300)
 
 
-# -------------------- 8) Coverage plot --------------------
-data, coverage = process_features_with_sufficient_coverage(features, feat_excl, settings, data_path)
+# # -------------------- 8) Coverage plot --------------------
+# data, coverage = process_features_with_sufficient_coverage(features, feat_excl, settings, data_path)
 
 
 # -------------------- 9) Implementable  Efficient Frontier --------------------
